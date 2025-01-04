6d3fdfa977567c9506fdf298129bc0a6c32ab6e7
Ohad Dekel <dekelohad@gmail.com>
2025-01-04 22:47:05 +0200

    fixed service cards

--- src/components/home-one/services/ServiceCard.jsx
+++ src/components/home-one/services/ServiceCard.jsx
@@ -1,19 +1,33 @@
 import { Link } from "react-router-dom";
 import ArrowRightImg from "../../../assets/images/icon/arrow-right.svg";
-function ServiceCard({ service: { title, description, icon } }) {
+import "./ServiceCard.css";
+
+function ServiceCard({ service: { title, description, icon, id } }) {
+	const getServiceRoute = (title) => {
+		const routes = {
+			"Paid Search Marketing": "/paid-search-marketing",
+			"Search Engine Optimization": "/search-engine-optimization",
+			"Local Lead Generation": "/local-lead-generation",
+			"Google Business Profile": "/google-my-business",
+			"Web Design": "/web-design",
+			"Web Development": "/web-development"
+		};
+		return routes[title] || "/services";
+	};
+
 	return (
-		<div className="aximo-iconbox-wrap">
+		<Link to={getServiceRoute(title)} className="aximo-iconbox-wrap" style={{ cursor: 'pointer', display: 'block', textDecoration: 'none' }}>
 			<div className="aximo-iconbox-icon">
 				<i className={`${icon}`}></i>
 			</div>
 			<div className="aximo-iconbox-data">
 				<h3>{title}</h3>
 				<p>{description}</p>
-				<Link to="/single-service" className="aximo-icon">
+				<span className="aximo-icon">
 					<img src={ArrowRightImg} alt="arrow right" />
-				</Link>
+				</span>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
--- src/components/home-one/services/index.jsx
+++ src/components/home-one/services/index.jsx
@@ -8,9 +8,9 @@ function Services({ services }) {
 			<div className="container">
 				<div className="aximo-section-title center">
 					<h2>
-						We provide effective
+						Elevate Your Business With
 						<span className="aximo-title-animation">
-							design solutions
+							Our Services
 							<span className="aximo-title-icon">
 								<img src={Star2Img} alt="Star2Img" />
 							</span>
