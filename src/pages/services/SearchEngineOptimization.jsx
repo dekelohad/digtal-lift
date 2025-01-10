62f04c7fd80dbc9a9811207e2fe00536b1eff49b
Ohad Dekel <dekelohad@gmail.com>
2025-01-10 15:02:11 +0200

    almost

--- src/pages/services/SearchEngineOptimization.jsx
+++ src/pages/services/SearchEngineOptimization.jsx
@@ -213,7 +213,7 @@ function SearchEngineOptimization() {
 									<svg className="w-24 h-24 mx-auto" viewBox="0 0 24 24">
 										<path
 											d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
-											className="fill-[#7dff00] drop-shadow-[0_0_8px_rgba(125,255,0,0.8)]"
+											className="fill-[#CCFF00] drop-shadow-[0_0_8px_rgba(204,255,0,0.8)]"
 										/>
 									</svg>
 								</div>
@@ -230,7 +230,7 @@ function SearchEngineOptimization() {
 									<svg className="w-24 h-24 mx-auto" viewBox="0 0 24 24">
 										<path
 											d="M7 2v11h3v9l7-12h-4l4-8H7zm4 4h3l-2 4h2l-3 5v-5H9V4h2v2z"
-											className="fill-[#7dff00] drop-shadow-[0_0_8px_rgba(125,255,0,0.8)]"
+											className="fill-[#CCFF00] drop-shadow-[0_0_8px_rgba(204,255,0,0.8)]"
 										/>
 									</svg>
 								</div>
@@ -247,7 +247,7 @@ function SearchEngineOptimization() {
 									<svg className="w-24 h-24 mx-auto" viewBox="0 0 24 24">
 										<path
 											d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"
-											className="fill-[#7dff00] drop-shadow-[0_0_8px_rgba(125,255,0,0.8)]"
+											className="fill-[#CCFF00] drop-shadow-[0_0_8px_rgba(204,255,0,0.8)]"
 										/>
 									</svg>
 								</div>
