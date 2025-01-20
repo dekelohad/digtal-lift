736c3eba70a98cd3f6cde3d5c1a4e3a98e3d99a2
Ohad Dekel <o-odekel@mekorot.co.il>
2025-01-20 15:53:43 +0200

    fixed services hamburger menu

--- src/components/common/navigation/desktop-nav/NavItem.jsx
+++ src/components/common/navigation/desktop-nav/NavItem.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function NavItem({ dropdown, title, children, url = "", setIsMobileMenuOpen }) {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 	const urlFormatted = url.startsWith('/') ? url : `/${url}`;
 
 	const handleClick = () => {
@@ -10,9 +12,14 @@ function NavItem({ dropdown, title, children, url = "", setIsMobileMenuOpen }) {
 		}
 	};
 
+	const handleDropdownClick = (e) => {
+		e.preventDefault();
+		setIsDropdownOpen(!isDropdownOpen);
+	};
+
 	return dropdown ? (
-		<li className="nav-item nav-item-has-children">
-			<a href="#" className="nav-link-item drop-trigger" onClick={handleClick}>
+		<li className={`nav-item nav-item-has-children ${isDropdownOpen ? 'dropdown-active' : ''}`}>
+			<a href="#" className="nav-link-item drop-trigger" onClick={handleDropdownClick}>
 				{title}
 				<i className="fas fa-angle-down"></i>
 			</a>
