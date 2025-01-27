cccdc7a9097d08043d44439e7d21d4511daff250
Ohad Dekel <o-odekel@mekorot.co.il>
2025-01-27 17:35:30 +0200

    added aron photo

--- src/data/teamsData.js
+++ src/data/teamsData.js
@@ -2,25 +2,13 @@ export const teamsData = [
     {
         id: crypto.randomUUID(),
         name: "Jonathan Dekel",
-        designation: "Chief Executive Officer",
-        img: "https://ohaddekel.dev/profileImage.jpg"
+        designation: "CEO & Technical SEO Director",
+        img: "/src/assets/images/team/Jonathan.jpg"
     },
     {
         id: crypto.randomUUID(),
-        name: "David Mitchell",
-        designation: "Chief Technology Officer",
-        img: "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=500&auto=format&fit=crop"
+        name: "Aron Terracina",
+        designation: "Director of Sales & Client Success",
+        img: "/src/assets/images/team/Aron.jpg"
     },
-    {
-        id: crypto.randomUUID(),
-        name: "James Wilson",
-        designation: "Head of Client Success",
-        img: "https://images.unsplash.com/photo-1537511446984-935f663eb1f4?q=80&w=500&auto=format&fit=crop"
-    },
-    {
-        id: crypto.randomUUID(),
-        name: "Mark Anderson",
-        designation: "Lead Generation Strategist",
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=500&auto=format&fit=crop"
-    }
 ]; 
